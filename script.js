0561998f4bd21c01a4f970948a1a82e704b9eb88
Mishka56 <mishka_19@icloud.com>
2025-01-25 22:22:07 +0100

    local

---
+++ script.js
@@ -0,0 +1,69 @@
+
+// let username = "Vastly";
+
+// const message = `My name ${username} have is ${username.length} length!`;
+
+// console.log(message);
+
+// console.log(username);
+
+// console.log(username[username.length - 1]);
+
+
+// const info = "My name Mykhailo";
+// const infoLength = info.length;
+// const firstElement = info[0];
+// const lastElement = info[info.length - 1];
+// console.log(info);
+
+// const price = 34599.5;
+
+// const priceTwo = 37600;
+
+// const priceThree = 13.654;
+
+// const numerical = Number.parseFloat(price);
+
+// let mathematicLogic = Math.floor(price);
+
+// console.log(Math.ceil(price));
+
+// console.log(Math.round(price));
+
+// console.log(Math.max(price, priceTwo, priceThree));
+ 
+// console.log(Math.min(price, priceTwo, priceThree));
+
+
+// // Дроби
+
+// // Метод 1:
+
+// const drabs = 0.1;
+
+// const drabs2 = 0.2;
+
+// console.log((drabs * 10 + drabs2 * 10) / 10); // 1 + 2 = 3 / 10 = 0.3  
+
+
+
+
+// // Метод 2:
+
+// console.log((drabs + drabs2).toFixed(1)); // 0.3
+
+// console.log((drabs + drabs2).toFixed(2)); // 0.30
+
+// console.log(drabs);
+
+const age = "Ваш вік?";
+
+if(Number.parseInt(age) === 27) {
+	console.log("Уже майже трицятка лобу");
+} else {
+	console.log("Соси");
+}
+
+console.log(age);
+
+
